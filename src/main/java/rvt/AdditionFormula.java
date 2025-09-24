6f8ab0e6af208282040a736592c43f3086784e0c
24DP3PPaeg <A240737PP@rvt.lv>
2025-09-24 14:53:03 +0300

    Addition Formula

---
+++ src/main/java/rvt/AdditionFormula.java
@@ -0,0 +1,19 @@
+package rvt;
+
+import java.util.Scanner;
+
+public class AdditionFormula {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Give the first number: ");
+        int numberone = Integer.valueOf(scanner.nextLine());
+        System.out.println("Give the second number: ");
+        int numbertwo = Integer.valueOf(scanner.nextLine());
+
+        int results = numberone + numbertwo;
+
+        System.out.println( numberone + "+" + numbertwo + "=" + results);
+        scanner.close();
+    }
+}
