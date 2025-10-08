18a0a386d155e2f9526326d0c878b7dfd431d012
24DP3PPaeg <A240737PP@rvt.lv>
2025-10-08 14:57:16 +0300

    Sum of sequence - the sequel

--- src/main/java/rvt/SumOfASequence.java
+++ src/main/java/rvt/SumOfASequence.java
@@ -5,10 +5,12 @@ import java.util.Scanner;
 public class SumOfASequence {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("First number: ");
+        int first = Integer.valueOf(scanner.nextLine());
         System.out.println("Last number: ");
         int last = Integer.valueOf(scanner.nextLine());
         int num = 0;
-        for (int i = 0; i <= last; i += 1) {
+        for (int i = first; i <= last; i += 1) {
                 num = num + i;
         }
         
