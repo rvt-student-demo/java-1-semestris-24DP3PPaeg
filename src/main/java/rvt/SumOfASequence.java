6a0591e6237076ef0bbbd8d69f83112c2277bcaa
24DP3PPaeg <A240737PP@rvt.lv>
2025-10-08 14:55:21 +0300

    Sum of a sequence

---
+++ src/main/java/rvt/SumOfASequence.java
@@ -0,0 +1,18 @@
+package rvt;
+
+import java.util.Scanner;
+
+public class SumOfASequence {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Last number: ");
+        int last = Integer.valueOf(scanner.nextLine());
+        int num = 0;
+        for (int i = 0; i <= last; i += 1) {
+                num = num + i;
+        }
+        
+        System.out.println("The sum is " + num);
+        scanner.close();
+    }
+}
