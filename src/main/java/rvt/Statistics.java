fda2291e24b21357b4e7c3be6d1800befd69ca65
24DP3PPaeg <A240737PP@rvt.lv>
2025-11-13 14:47:09 +0200

    Statistics version 2, 4 parts

--- src/main/java/rvt/Statistics.java
+++ src/main/java/rvt/Statistics.java
@@ -27,3 +27,37 @@ public class Statistics {
         return this.sum / (double) this.count;
     }
 }
+
+// public static void main(String[] args) {
+//         Scanner scanner = new Scanner(System.in);
+
+//         Statistics statistics = new Statistics();
+//         Statistics evenStat = new Statistics();
+//         Statistics oddStat = new Statistics();
+//         // statistics.addNumber(3);
+//         // statistics.addNumber(5);
+//         // statistics.addNumber(1);
+//         // statistics.addNumber(2);
+        
+
+//         System.out.println("Give me numbers: ");
+//         while (true){
+//             int number = Integer.valueOf(scanner.nextLine());
+//             if (number == -1){
+//                 break;
+//             } else{
+//                 statistics.addNumber(number);
+//             }
+
+//             if (number % 2 == 0){
+//                 evenStat.addNumber(number);
+//             }
+//             else if (number % 2 != 0){
+//                 oddStat.addNumber(number);
+//             }
+//         }
+//         // System.out.println("Count: " + statistics.getCount());
+//         System.out.println("Sum: " + statistics.sum());
+//         // System.out.println("Average: " + statistics.average());
+//         System.out.println("Sum of even number: " + evenStat.sum());
+//         System.out.println("Sum of odd numbers: " + oddStat.sum());
