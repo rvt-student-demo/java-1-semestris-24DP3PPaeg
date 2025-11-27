df04bdd54ae1ad646bf874e23d6249800755e8e0
24DP3PPaeg <A240737PP@rvt.lv>
2025-11-27 11:24:52 +0200

    Pyment card (completed)

--- src/main/java/rvt/PaymentCard.java
+++ src/main/java/rvt/PaymentCard.java
@@ -30,6 +30,29 @@ public class PaymentCard {
     }
 
     public void addMoney(double amount) {
-        balance += amount;
+        if (amount < 0) {
+            ;
+        } else {
+            balance += amount;
+        }
     }
 }
+
+// public static void main(String[] args) {
+//         PaymentCard paulsCard = new PaymentCard(20);
+//         PaymentCard mattsCard = new PaymentCard(30);
+
+//         paulsCard.eatHeartily();
+//         mattsCard.eatAffordably();
+//         System.out.println("Pauls: " + paulsCard);
+//         System.out.println("Matts: " + mattsCard);
+//         paulsCard.addMoney(20);
+//         mattsCard.eatHeartily();
+//         System.out.println("Pauls: " + paulsCard);
+//         System.out.println("Matts: " + mattsCard);
+//         paulsCard.eatAffordably();
+//         paulsCard.eatAffordably();
+//         mattsCard.addMoney(50);
+//         System.out.println("Pauls: " + paulsCard);
+//         System.out.println("Matts: " + mattsCard);
+//     }
